7fbba12b7ec0eb5710bf6d98cb8487ec51270032
Ng Joon Han <capriconz0109@gmail.com>
2026-04-16 14:03:10 +0800

    Create 02_create_inflation_clean.sql

---
+++ sql/02_create_inflation_clean.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE TABLE
+`project-9c13f7a9-4ab5-468e-8ef.post_covid_analysis.inflation_clean` AS
+
+SELECT
+  Country_Name,
+  Country_Code,
+  Indicator_Name,
+  Indicator_Code,
+  _2018,
+  _2019,
+  _2020,
+  _2021,
+  _2022,
+  _2023
+FROM `project-9c13f7a9-4ab5-468e-8ef.post_covid_analysis.inflation_raw`
+WHERE Country_Name IN (
+  'China',
+  'France',
+  'Germany',
+  'Singapore',
+  'United States'
+);
